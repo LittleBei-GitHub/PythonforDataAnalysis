9f9fa3e478c19fe39853f4695d729e8db072c24e
莫小苝 <lovejing0306@gmail.com>
2016-10-10 15:25:40 +0800

    update json_data.py

--- Chapter-02/json_data.py
+++ Chapter-02/json_data.py
@@ -71,4 +71,15 @@ print(tz_counts[:10])
 # tz_counts[:10].plot(kind='barh', rot=0)
 # plt.show()
 
-#
+# 合并操作
+cframe = frame[frame.a.notnull()]
+operating_system = np.where(
+    cframe['a'].str.contains('Windows'), 'Windows', 'Not Windows') # where 相当于一个循环判别函数
+#print(operating_system[:5])
+by_tz_os = cframe.groupby(['tz', operating_system])
+agg_counts = by_tz_os.size().unstack().fillna(0)
+print(agg_counts[:10])
+indexer = agg_counts.sum(1).argsort()
+print(indexer[:10])
+count_subset = agg_counts.take(indexer)[-10:]
+print(count_subset)
