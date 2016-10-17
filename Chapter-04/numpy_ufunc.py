f8a5e11540a43861e50bdfffb10e557cf8b95785
莫小苝 <lovejing0306@gmail.com>
2016-10-17 16:46:33 +0800

    create numpy_ufunc.py

---
+++ Chapter-04/numpy_ufunc.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+import numpy as np
+
+if __name__ == '__main__':
+    print('通用函数：快速的元素级数组函数')
+    arr = np.arange(10)
+    print(arr)
+    print(np.sqrt(arr))
+    print(np.exp(arr))
+
+    x = np.random.randn(8)
+    y = np.random.randn(8)
+    print(x)
+    print(y)
+    print(np.maximum(x, y))  # 元素级最大值
+
+    arr = np.random.randn(7) * 5
+    print(np.modf(arr))
