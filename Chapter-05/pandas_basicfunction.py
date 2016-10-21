433581602bfa6a79f250b7e8e6cd489e52c28f79
莫小苝 <lovejing0306@gmail.com>
2016-10-21 21:38:17 +0800

    update chapter-05

--- Chapter-05/pandas_basicfunction.py
+++ Chapter-05/pandas_basicfunction.py
@@ -80,3 +80,76 @@ if __name__ == '__main__':
     print(obj[obj<2])
 
     print(obj['b':'c']) # 与普通的python切片运算不同，其末端是包含的
+
+    # DataFrame索引、选取和过滤
+    print('DataFrame索引、选取和过滤:')
+    data = DataFrame(np.arange(16).reshape((4, 4)),
+                     index=['Ohio', 'Colorado', 'Utah', 'New York'],
+                     columns=['one', 'two', 'three', 'four'])
+    print(data)
+    print(data['two'])
+    print(data[['two', 'one']])
+    print(data[:2]) # 通过切片的方式访问
+    print(data[data['three']>5]) # 通过布尔索引的方式访问
+    print(data<5)
+    data[data<5] = 0
+    print(data)
+
+    print(data.ix['Ohio']) # 访问行
+    print(data.ix[0])
+
+    print(data.ix['Colorado', ['two', 'three']]) # 访问行、列
+    print(data.ix[['Colorado', 'Utah'], ['two', 'three']])
+    print(data.ix[['Colorado', 'Utah'], [1, 2]])
+    print(data.ix[:'Utah', 'two'])
+    print(data.ix[data.three>5, :3])
+
+    ## 算术运算和数据对齐
+    # Series算术运算和数据对齐
+    s1 = Series([7.3, -2.5, 3.4, 1.5], index=['a', 'c', 'd', 'e'])
+    s2 = Series([-2.1, 3.6, -1.5, 4, 3.1], index=['a', 'c', 'e', 'f', 'g'])
+    print(s1+s2)
+
+    # DataFrame算术运算和数据对齐
+    df1 = DataFrame(np.arange(9).reshape((3, 3)),
+                    columns=['b', 'c', 'd'],
+                    index=['Ohio', 'Texas', 'Colorado'])
+    df2 = DataFrame(np.arange(12).reshape((4, 3)),
+                    columns=list('bde'),
+                    index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+    print(df1)
+    print(df2)
+    print(df1+df2)
+
+    # 在算术方法中填充值
+    df1 = DataFrame(np.arange(12).reshape((3, 4)), columns=list('abcd'))
+    df2 = DataFrame(np.arange(20).reshape((4, 5)), columns=list('abcde'))
+    print(df1)
+    print(df2)
+    print(df1+df2)
+    print(df1.add(df2, fill_value = 0))
+    print(df1.reindex(columns=df2.columns, fill_value=0))
+
+    ##DataFrame和Series之间的运算
+    print('numpy上的广播：')
+    arr = np.arange(12).reshape((3, 4))
+    print(arr)
+    print(arr[0])
+    print(arr-arr[0]) # 这就是广播
+
+    print('Series和DataFrame的广播：')
+    frame = DataFrame(np.arange(12).reshape((4, 3)),
+                    columns=list('bde'),
+                    index=['Utah', 'Ohio', 'Texas', 'Oregon'])
+    series = frame.ix[0]
+    print(frame)
+    print(series)
+    print(frame-series) # 匹配列在行上广播，算术运算会将Series的索引匹配到DataFrame的列
+
+    series2 = Series(range(3), index=['b', 'e', 'f'])
+    print(frame+series2) # 如果某个索引值在DataFrame的列或Series的索引上找不见，则以NaN代替
+
+    series3 = frame['d']
+    print(frame)
+    print(series3)
+    print(frame.sub(series3, axis=0)) # 匹配行在列上广播
