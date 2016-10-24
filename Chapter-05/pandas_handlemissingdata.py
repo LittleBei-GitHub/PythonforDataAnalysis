faecb29f63a786394e8671636d94793cf8efe33e
莫小苝 <lovejing0306@gmail.com>
2016-10-24 20:14:55 +0800

    update pandas_handlemissingdata.py

--- Chapter-05/pandas_handlemissingdata.py
+++ Chapter-05/pandas_handlemissingdata.py
@@ -3,3 +3,40 @@
 from pandas import DataFrame, Series
 import pandas as pd
 import numpy as np
+
+# 测试
+if __name__ == '__main__':
+    string_data = Series(['aardvark', 'artichoke', np.nan, 'avocado'])
+    print(string_data)
+    print(string_data.isnull())
+    string_data[0] = None # python内置的None值也会被当作NaN来处理
+    print(string_data.isnull())
+    print(string_data.notnull())
+
+    ## 滤除缺失数据
+    # Series
+    data = Series([1, np.nan, 3.5, np.nan, 7])
+    print(data)
+    print(data.dropna())
+    print(data[data.notnull()]) # 通过布尔索引达到相同的效果
+
+    # DataFrame
+    data = DataFrame([[1., 6.5, 3.],
+                      [1., np.nan, np.nan],
+                      [np.nan, np.nan, np.nan, np.nan],
+                      [np.nan, 6.5, 3.]])
+    print(data)
+    print(data.dropna()) # 默认丢弃含有NaN的行
+    print(data.dropna(how='all')) # 丢弃全部都是NaN的行
+
+    data[4] = np.nan
+    print(data)
+    print(data.dropna(axis=1)) # 丢弃列
+    print(data.dropna(axis=1, how='all'))
+
+    df = DataFrame(np.random.randn(7,3))
+    df.ix[:4, 1] = np.nan
+    df.ix[:2, 2] = np.nan
+    print(df)
+    print(df.dropna())
+    print(df.dropna(thresh=3))
