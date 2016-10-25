54979d227dc6586e9dc26ad7c2681c371e2a76b2
莫小苝 <lovejing0306@gmail.com>
2016-10-25 15:15:00 +0800

    update pandas_handlemissingdata.py

--- Chapter-05/pandas_handlemissingdata.py
+++ Chapter-05/pandas_handlemissingdata.py
@@ -40,3 +40,23 @@ if __name__ == '__main__':
     print(df)
     print(df.dropna())
     print(df.dropna(thresh=3))
+
+    ## 填充缺失数据
+    print(df.fillna(0))
+
+    print(df.fillna({1:0.5, 2:-1})) # 通过字典实现对不同列不同值的填充
+
+    dfInplace = df.fillna(0, inplace=True)
+    print(dfInplace) # fillna默认返回新对象，但也可以对现有对象进行就地修改
+    print(df)
+
+    df = DataFrame(np.random.randn(6,3))
+    print(df)
+    df.ix[2:, 1] = np.nan
+    df.ix[3:, 2] = np.nan
+    print(df)
+    print(df.fillna(method='ffill'))
+    print(df.fillna(method='ffill', limit=2))
+
+    data = Series([1., np.nan, 3.5, np.nan, 7])
+    print(data.fillna(data.mean()))
